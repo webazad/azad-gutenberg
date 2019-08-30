b8bd6d4fa6c11a2316ac717d720ffb80660c8be3
Azad <azad@azad.com>
2019-08-30 21:08:21 +0600

    Added.

--- azad-gutenberg.js
+++ azad-gutenberg.js
@@ -1,4 +1,24 @@
-wp.blocks.registerBlockType('azad/azad-gutenberg',{
+//wp.blocks.registerBlockType('azad/azad-gutenberg',{
+//    title: 'Azad Gutenberg',
+//    icon: 'smiley',
+//    category: 'common',
+//    attributes: {
+//        content:{type:'string'},
+//        color:{type:'string'}
+//    },
+//    edit: function(props){
+//        return wp.element.createElement(
+//            'h3',
+//            null,
+//            'asdf'
+//        );
+//    },
+//    save: function(props){
+//        return null;
+//    }
+//});
+
+wp.blocks.registerBlockType('azad/azad-blocks',{
     title: 'Azad Gutenberg',
     icon: 'smiley',
     category: 'common',
--- azad-gutenberg.php
+++ azad-gutenberg.php
@@ -12,26 +12,61 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: azad-gutenberg
 Domain Path: /languages
 */
-if(! defined('ABSPATH')) exit;
-class Azad_Gutenberg{
-    public function __construct(){
-        add_action('plugins_loaded',array($this,'constants'),1);
-    }
-    public function constants(){
-        //echo 'Constants';
-    }
-    public function i18n(){
-        //echo 'i18n';
-    }
-    public function includes(){
-        define('ADMIN',plugin_dir_path(__FILE__));
-        require_once(ADMIN.'inc/functions.php');
-    }
-    public function admin(){
-        if(is_admin()){
-            require_once(ADMIN.'admin/admin.php');            
+//if(! defined('ABSPATH')) exit;
+//function azad_gutenberg_one(){
+//    wp_enqueue_script('azad-gutenberg',
+//        plugins_dir_url(__FILES__).'azad-gutenberg.js',
+//            array('wp-blocks','wp-i18n','wp-editor'),true);
+//}
+//add_action('enqueue_block_editor_assets','azad_gutenberg_one');
+
+// Check for gutenberg
+if(function_exists('the_gutenberg_project')){
+    if(! function_exists('azad_gutenberg')){
+        function azad_gutenberg(){
+            wp_register_script(
+                'azad-gutenberg-one',
+                plugins_url('azad-gutenberg.js',__FILES__),
+                array('wp-blocks','wp-element'),
+                true
+            );
+            wp_register_style(
+                'azad-gutenberg-two',
+                plugins_url('azad-gutenberg.css',__FILES__),
+                array('wp-edit-blocks','wp-element'),
+                '1234',
+                'all'
+            );
+            wp_register_style(
+                'azad-gutenberg-three',
+                plugins_url('azad-gutenberg.css',__FILES__),
+                array('wp-edit-blocks','wp-element'),
+                '1234',
+                'all'
+            );
+            register_block_type('azad/azad-block',array(
+                    'editor_script' => 'azad-gutenberg-one',
+                    'editor_style' => 'azad-gutenberg-two',
+                    'style' => 'azad-gutenberg-three'
+                )             
+            );
         }
     }
-    public function __destruct(){}
 }
-new Azad_Scroll_Top();
+add_action('init','azad_gutenberg');
+/*
+if(! class_exists('Azad_Gutenberg')){
+    class Azad_Gutenberg{
+        public function __construct(){
+            add_action('plugins_loaded',array($this,'constants'),1);
+        }
+        public function admin(){
+            if(is_admin()){
+                require_once(ADMIN.'admin/admin.php');            
+            }
+        }
+        public function __destruct(){}
+    }
+}
+new Azad_Gutenberg();
+*/
